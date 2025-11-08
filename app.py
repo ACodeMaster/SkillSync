8731a1a7e770659c60e79cce8de88a5fa5d84e00
Anand <dhaygudeanand8@gmail.com>
2025-11-09 01:38:58 +0530

    Fixed host, port, and added Procfile

--- app.py
+++ app.py
@@ -704,9 +704,10 @@ def resume_builder():
 # ----------------------------
 # Run App
 # ----------------------------
+
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
