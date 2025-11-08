11f28249d37dd5f388abeecb82e5418a1c1a2771
Anand <dhaygudeanand8@gmail.com>
2025-11-09 01:53:39 +0530

    Fixed Flask host and port for Render

--- app.py
+++ app.py
@@ -708,6 +708,5 @@ def resume_builder():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=False)
